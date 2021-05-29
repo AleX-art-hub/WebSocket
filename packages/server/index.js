4944f4b3828eda388e55b4da4e2884e13481cd10
BodNS <bondarenokn@gmail.com>
2021-05-29 13:58:56 +0300

    io.socket

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,26 +1,39 @@
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const app = express();
-const expressWs = require('express-ws')(app);
-const allWss = expressWs.getWss('/');
-app.use(express.json());
+const server = http.Server(app);
+const io = socketIO (server);
+const router = require('./router');
+app.use(router);
+io.on('connection', function(socket){
+  socket.on('test', (data, options) => {
+    console.log('data:', data);
+    console.log('options:', options);
+  });
+});
+//const expressWs = require('express-ws')(app);
+//const allWss = expressWs.getWss('/');
+//app.use(express.json());
 
 //сообщения
-const messages = [];
+// const messages = [];
 
-app.ws('/', function (ws, req) {
-  //работаем с сообщениями
-  ws.send(JSON.stringify(messages));
-  ws.on('message', function (msg) {
-    console.log(msg);
-    // добавляем сообщение
-    messages.push(msg);
-    // оповещаем всех клиентов
-    allWss.clients.forEach(function (client) {
-      client.send(JSON.stringify([msg]));
-    });
-  });
-  console.log('socket', req.testing);
-});
+// app.ws('/', function (ws, req) {
+//   //работаем с сообщениями
+//   ws.send(JSON.stringify(messages));
+//   ws.on('message', function (msg) {
+//     console.log(msg);
+//     // добавляем сообщение
+//     messages.push(msg);
+//     // оповещаем всех клиентов
+//     allWss.clients.forEach(function (client) {
+//       client.send(JSON.stringify([msg]));
+//     });
+//   });
+//   console.log('socket', req.testing);
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
