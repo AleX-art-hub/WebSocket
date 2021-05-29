0ec8eaedfaba40cdb7c3c22ddc05a5311998d9f7
BodNS <bondarenokn@gmail.com>
2021-05-29 13:08:13 +0300

    server for many clients

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
+
+const allWss = expressWs.getWss('/');
+
 app.use(express.json());
+
 app.ws('/', function(ws,req){
 ws.on('message', function(msg){
     console.log(msg);
+    allWss.clients.forEach(function(client){
+        client.send(msg.data);
+    });
 });
 console.log('socket', req.testing);
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
